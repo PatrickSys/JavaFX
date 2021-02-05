243008260b8f9fb68d634690ed33ce0776e9a4e4
PatrickSys <rossellocolompatrick@gmail.com>
2021-02-05 19:03:18 +0100

    starting

---
+++ src/Pong/MyPong.java
@@ -0,0 +1,20 @@
+package Pong;
+
+/************************************************************************
+ Made by        PatrickSys
+ Date           05/02/2021
+ Package        Pong
+ Description:
+ ************************************************************************/
+
+
+public class MyPong {
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
---
+++ src/Pong/RectangleTeclat.java
@@ -0,0 +1,43 @@
+package Pong;
+
+
+/************************************************************************
+ Made by        PatrickSys
+ Date           05/02/2021
+ Package        Pong
+ Description:
+ ************************************************************************/
+
+import javafx.scene.Node;
+import javafx.scene.layout.Pane;
+
+public class RectangleTeclat {
+
+
+    class Rectangle {
+
+        class Posicio {
+
+            int posX;
+            int posY;
+
+            public Posicio(int x, int y) {
+                this.posX = x;
+                this.posY = y;
+            }
+        }
+
+
+        Posicio posicio;
+        int altura;
+        int velocitat = 10;
+        Pane panell;
+        Node bolla;
+
+
+        //public Rectangle
+
+
+    }
+
+}

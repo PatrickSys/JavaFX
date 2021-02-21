ea6109e207ae005888558e0bc90663ca2a8e91a0
PatrickSys <prossello@nigul.cide.es>
2021-02-21 18:54:55 +0100

    Example to load custom fonts

--- src/Labels/testLabels.java
+++ src/Labels/testLabels.java
@@ -1,11 +1,7 @@
 package Labels;
 
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 
-import java.awt.*;
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
 import java.io.InputStream;
 
 /************************************************************************
@@ -16,56 +12,23 @@ import java.io.InputStream;
  ************************************************************************/
 
 
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
 public class testLabels  {
 
 
 
-        private static Font ttfBase = null;
-        private static Font telegraficoFont = null;
-        private static InputStream myStream = null;
-        private static final String FONT_PATH_TELEGRAFICO = "imageFolder/TELEGRAFICO.TTF";
-
-        public static Font createFont() {
-
-
-            try {
-                myStream = new BufferedInputStream(
-                        new FileInputStream("Labels/stocky.ttf"));
-                ttfBase = Font.createFont(Font.TRUETYPE_FONT, myStream);
-                telegraficoFont = ttfBase.deriveFont(Font.PLAIN, 24);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-                System.err.println("Font not loaded.");
-            }
-            return telegraficoFont;
-        }
-
-
-    public static Font font = null;
 
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    if (font == null) {
-                        font = createFont();
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                Pane pane = new Pane();
-                pane.setStyle("-fx-background-color: BLACK");
-                Scene scene = new Scene(pane, 500, 500);
-                Label label = new Label("estoejemplo");
-                label.setBounds(50, 50, 50, 50);
-                label.setLocation(50,50);
-                label.setFont(font);
 
-            } // public void run() Closing
-        });
     }
 
 }
-
-
-
